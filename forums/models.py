3ba5fe19c35c7f73d3890e9c77d472dcb30f32f4
erikwilbert <erik.wilbert@ui.ac.id>
2025-10-08 16:21:23 +0700

    Add: models of forums and replies

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Forums(models.Model):
+    forums_id = models.UUIDField(auto_created=True, primary_key=True)
+    # user
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    forums_views = models.PositiveIntegerField(default=0)
+    forums_likes = models.PositiveIntegerField(default=0)
+    forums_replies_counts = models.PositiveIntegerField(default=0)
+    is_hot = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    def increment_views(self):
+        self.forums_views += 1
+        self.save()
+
+class ForumsReplies(models.Model):
+    forums_id = models.ForeignKey(Forums, on_delete=models.CASCADE, related_name="forums")
+    # user
+    replies_content = models.TextField()
+    forums_replies_likes = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return super().__str__()
+    
