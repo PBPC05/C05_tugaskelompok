3fdfbff19c8e0a926c8f8bab7ffa5dfc1a5b5c6f
arya.putra41 <aryakesit4@gmail.com>
2025-10-22 19:48:04 +0700

    Commit to fix merge errors

--- apps/prediction/views.py
+++ apps/prediction/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from apps.prediction.models import PredictionVote
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def show_main(request):
 
-    return render(request, "prediction.html")
+    return render(request, "prediction_main.html")
+
+def get_votes_json(request):
+    try:
+        votes_list = PredictionVote.objects.all()
+        data = [
+            {
+                'type': vote.type,
+                'race': vote.race,
+                'content': vote.content
+            }
+            for vote in votes_list
+        ]
+
+        return JsonResponse(data, safe=False)
+    except:
+        print("Error")
+
+@require_POST
+def post_vote(request):
+    type = request.POST.get("type")
+    race = request.POST.get("race")
+    content = request.POST.get("type")
+
+    vote = PredictionVote(
+        type = type,
+        race = race,
+        content = content,
+    )
+    vote.save()
+
+    response = redirect('prediction_main.html')
+    return response
