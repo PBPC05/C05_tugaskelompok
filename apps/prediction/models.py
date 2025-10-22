257d31a60bf19f84b699d6cda7770209c0f311e6
arya.putra41 <aryakesit4@gmail.com>
2025-10-22 19:48:04 +0700

    Commit to fix merge errors

--- apps/prediction/models.py
+++ apps/prediction/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class PredictionVote(models.Model):
+    TYPE_SELECTIONS = {
+        'driver': "Driver",
+        'team': "Team"
+    }
+
+    # user
+    type = models.CharField(max_length=20, choices=TYPE_SELECTIONS, default="driver")
+    race = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
--- apps/prediction/urls.py
+++ apps/prediction/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.prediction.views import show_main
+from apps.prediction.views import show_main, post_vote
 
 app_name = 'prediction'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('post_vote', post_vote, name='post_vote')
 ]
